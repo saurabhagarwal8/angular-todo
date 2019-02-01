cc3bf7d91a0cf012b7ff0f9406414a48ab883fbe
saurabhagarwal8 <saurabh28a@gmail.com>
2019-02-01 22:36:40 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { makeDecorator } from '@angular/core/src/util/decorators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  todos = [{"todo":"Study","done":false},{"todo":"Cleaning","done":false}];
+  todo="";
+
+  add(){
+    if(this.todo=='' || this.todo==null)
+    alert("To-Do Empty!");
+    else
+    this.todos.push({"todo":this.todo,"done":false})
+    console.log(this.todos);
+  }
+
+  done(index){
+    if(this.todos[index].done==false)
+    this.todos[index].done=true;
+    else
+    this.todos[index].done=false;
+    console.log(this.todos)
+
+  }
+
+  remove(index){
+
+      this.todos[index] = null;
+      this.todos.splice(index,1);
+     console.log("Deleted "+this.todos+index);
+  }
 }
